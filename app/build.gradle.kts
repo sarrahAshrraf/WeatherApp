d95db6525f2a604eabf490bb18bdb6ad3306b8f9
sarrahAshrraf <sarahsoou01@gmail.com>
2024-03-10 20:37:47 +0200

    done splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 }
 
 android {
+    viewBinding{ enable= true}
+    dataBinding{enable = true}
     namespace = "com.example.weatherapppoject"
     compileSdk = 34
 
@@ -44,4 +46,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //lottifie
+    implementation ("com.airbnb.android:lottie:5.2.0")
+    //glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
--- app/src/main/java/com/example/weatherapppoject/view/MainActivity.kt
+++ app/src/main/java/com/example/weatherapppoject/view/MainActivity.kt
@@ -3,11 +3,37 @@ package com.example.weatherapppoject.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.weatherapppoject.R
+import com.airbnb.lottie.LottieAnimationView
+import com.bumptech.glide.Glide
+import com.example.weatherapppoject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.animationView.playAnimation()
 
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.wordsplashscreen)
+            .into(binding.imageView)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.animationView.resumeAnimation()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.animationView.pauseAnimation()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.animationView.cancelAnimation()
     }
 }
