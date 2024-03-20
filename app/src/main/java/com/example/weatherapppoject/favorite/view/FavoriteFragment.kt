518fd76fbf90671627685c4a45183035bc05777b
sarrahAshrraf <sarahsoou01@gmail.com>
2024-03-21 01:33:13 +0200

    add alert dialog to delete the favorite items

--- app/src/main/java/com/example/weatherapppoject/favorite/view/FavoriteFragment.kt
+++ app/src/main/java/com/example/weatherapppoject/favorite/view/FavoriteFragment.kt
@@ -1,5 +1,6 @@
 package com.example.weatherapppoject.favorite.view
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -89,9 +90,21 @@ class FavoriteFragment : Fragment() {
 
         favAdapter = FavoritesAdapter(emptyList(),
             { product, position ->
+                val alertDialogBuilder = AlertDialog.Builder(context)
+                alertDialogBuilder.setTitle("Delete Confirmation")
+                alertDialogBuilder.setMessage("Are you sure you want to remove this item?")
+                alertDialogBuilder.setPositiveButton("OK") { dialog, _ ->
                 favoriteViewModel.removeFromFavorites(product)
+                    dialog.dismiss()
+                }
+                alertDialogBuilder.setNegativeButton("Cancel") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                val alertDialog = alertDialogBuilder.create()
+                alertDialog.show()
             },
             { product, position ->
+
                 // Navigate to the new fragment
 
                 val bundle = Bundle().apply {
